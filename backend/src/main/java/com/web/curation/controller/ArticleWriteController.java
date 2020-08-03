e4d1730c14635d2995b08b9258721f5fe40a26c5
riri <hyerin77@naver.com>
2020-08-03 14:53:13 +0900

    add articlewrite

--- backend/src/main/java/com/web/curation/controller/ArticleWriteController.java
+++ backend/src/main/java/com/web/curation/controller/ArticleWriteController.java
@@ -18,7 +18,9 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.web.curation.dto.BasicResponse;
+import com.web.curation.dto.article.Article;
 import com.web.curation.jwt.service.JwtService;
+import com.web.curation.service.articlewrite.ArticleWriteService;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jws;
@@ -38,7 +40,10 @@ public class ArticleWriteController {
 	@Autowired
 	private JwtService jwtService;
 	
-	@PostMapping("/writearticle")
+	@Autowired
+	private ArticleWriteService articleService;
+	
+	@PostMapping("/articlewrite")
     public ResponseEntity<Map<String,Object>> addFavtag(@RequestBody Map<String, Object> map, HttpServletRequest request) {
     	
 //		ArrayList<Integer> taglist = (ArrayList)map.get("taglist");
@@ -52,18 +57,35 @@ public class ArticleWriteController {
 		Jws<Claims> claims = jwtService.getDecodeToken(token);
 		Map<String, Object> Userinfo = new HashMap<String, Object>();
 		Userinfo = (Map<String, Object>) claims.getBody().get("AuthenticationResponse");
-		String email = Userinfo.get("email").toString();
-		String writer = Userinfo.get("nickname").toString();
+		String email = Userinfo.get("email").toString(); //이메일 
+		String writer = Userinfo.get("nickname").toString(); //닉네임 
+		String content = map.get("content").toString(); //글내용
+		String link = map.get("link").toString();
 
 		try {
+			
 			String baseDir ="C:\\tugetherimg";
         	MultipartFile files = (MultipartFile)map.get("file"); // 파일 받아오기 
         	String fileName = files.getOriginalFilename();
 			String filePath = baseDir + "\\" + fileName;
 			files.transferTo(new File(filePath));
 			
+			Article article = new Article();
+			article.setEmail(email);
+			article.setWriter(writer);
+			article.setImage(filePath);
+			article.setContent(content);
+			article.setLink(link);
+			articleService.addArticle(article);
+			resultMap.put("status", true);
+    		status = HttpStatus.OK;
+    		System.out.println("입력한 태그를 디비에 넣었습니다!!!!! ");
+			
 		} catch(Exception e) {
 			e.printStackTrace();
+			System.out.println("게시글 등록 실패 ");
+    		resultMap.put("message", e.getMessage());
+    		status = HttpStatus.INTERNAL_SERVER_ERROR;
 		}
 		
 		return new ResponseEntity<Map<String,Object>>(resultMap, status);
