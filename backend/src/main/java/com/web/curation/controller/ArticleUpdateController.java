2311b99b16933cb5ee05d7f15dcef3d11fee2389
riri <hyerin77@naver.com>
2020-08-11 11:51:05 +0900

    S03P13B303-43 [+]Add ArticleUpdateController

---
+++ backend/src/main/java/com/web/curation/controller/ArticleUpdateController.java
@@ -0,0 +1,28 @@
+package com.web.curation.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.web.curation.dto.BasicResponse;
+
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
+@ApiResponses(value = { @ApiResponse(code = 401, message = "Unauthorized", response = BasicResponse.class),
+        @ApiResponse(code = 403, message = "Forbidden", response = BasicResponse.class),
+        @ApiResponse(code = 404, message = "Not Found", response = BasicResponse.class),
+        @ApiResponse(code = 500, message = "Failure", response = BasicResponse.class) })
+
+@CrossOrigin(origins = { "*" })
+@RestController
+@RequestMapping("/tugether")
+public class ArticleUpdateController {
+
+//	@Value("${ubuntu.article.upload.directory}")
+	@Value("${window.article.upload.directory}")
+	String upload_FILE_PATH;
+	
+	
+}
